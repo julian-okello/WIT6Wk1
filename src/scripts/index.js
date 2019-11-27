92015e40d46fe08d75de7974adedc3f52a3c485d
Julian Okello <julian.okello@gmail.com>
2019-11-27 17:34:58 +0300

    Generating Object Challenge

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,15 @@
-const linkHeaders = [
-    { id: "userstory", title: "USER STORY" },
-    { id: "mockups", title: "DESIGN MOCKUPS" },
-    { id: "rr", title: "RINSE & REPEAT" },
-    { id: "development", title: "DEVELOPMENT" },
-    { id: "testing", title: "TESTING" },
-    { id: "deployment", title: "DEPLOYMENT" },
-];
+// const linkHeaders = [
+//     { id: "userstory", title: "USER STORY" },
+//     { id: "mockups", title: "DESIGN MOCKUPS" },
+//     { id: "rr", title: "RINSE & REPEAT" },
+//     { id: "development", title: "DEVELOPMENT" },
+//     { id: "testing", title: "TESTING" },
+//     { id: "deployment", title: "DEPLOYMENT" },
+// ];
 
 window.onload = function () {
     this.loadComponents();
-    this.loadNavItems();
+    // this.loadNavItems();
 }
 
 var loadComponents = function () {
@@ -17,10 +17,10 @@ var loadComponents = function () {
     $('#main-doc').load('./main.html');
 }
 
-var loadNavItems = function () {
-    var content = "<ul>";
-    for (var i = 0; i < linkHeaders.length; i++) {
-        content += "<li><a href='#" + linkHeaders[i]['id'] + "' rel='internal' class='nav-link'>" + linkHeaders[i]['title'] + "</a></li>";
-    }
-    $('#navlinks').html(content + "</ul>");
-}
+// var loadNavItems = function () {
+//     var content = "<ul>";
+//     for (var i = 0; i < linkHeaders.length; i++) {
+//         content += "<li><a href='#" + linkHeaders[i]['id'] + "' rel='internal' class='nav-link'>" + linkHeaders[i]['title'] + "</a></li>";
+//     }
+//     $('#navlinks').html(content + "</ul>");
+// }
